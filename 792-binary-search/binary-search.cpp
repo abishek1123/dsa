26b72808c809b2fff8d05cb8e70370705edd2541
abishek sr <abisheksundar1114@gmail.com>
2026-08-08 23:57:23 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (47.10%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -8,7 +8,7 @@ public:
 
         while (low <= high) {
 
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
 
             if (nums[mid] == target)
                 return mid;
